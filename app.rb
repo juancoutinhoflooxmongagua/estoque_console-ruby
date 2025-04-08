2262aa925698dcc05e7270deae5a0565c2b96a86
JuanCoutinho <juan.devx@gmail.com>
2025-04-08 15:57:06 -0300

    feat: create menu

---
+++ app.rb
@@ -0,0 +1,3 @@
+require_relative 'display/menu.rb'
+
+open_menu
---
+++ display/menu.rb
@@ -0,0 +1,25 @@
+def open_menu
+    loop do 
+      p "Options:"
+      p "1. Register Products"
+      p "2. Products List"
+      p "3. Back Product"
+      p "4. Back"
+  
+      option = gets.to_i
+  
+      case option
+      when 1
+        p "Cadastro"
+      when 2
+        p "Lista"
+      when 3
+        p "Retirada"
+      when 4
+        break
+      else
+        p "invalid option"
+      end
+    end
+  end
+  
