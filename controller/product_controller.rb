2b26f9f1273f866a1576e50517abbc0fce9008ad
JuanCoutinho <juan.devx@gmail.com>
2025-04-09 23:58:31 -0300

    feat: create list products

--- controller/product_controller.rb
+++ controller/product_controller.rb
@@ -1,8 +1,40 @@
 def create_product(products)
     message "Digite o nome do produto:"
-    nome = gets.chomp
+    name = gets.chomp
     
-    success_message("Produto '#{nome}' cadastrado com sucesso!")
+    message "Digite a descrição do produto #{name}:"
+    description = gets.chomp
+        
+    message "Digite o preço do produto #{name}:"
+    price = gets.to_f
+
+    message "Digite a quantidade de estoque do produto #{name}:"
+    qtd = gets.to_f
+
+    success_message("Produto '#{name}' cadastrado com sucesso!")
+
+    success_message("PRODUTO:
+    Nome: #{name}
+    Descrição: #{description}
+    Preço: #{price}
+    Quantidade Estoque: #{qtd}
+    ")
+    
+    products << {
+        id: Time.now.to_i
+        name: name,
+        description: description,
+        price: price,
+        qtd: qtd,
+    }
+
     pause
-  end
-  
+end
+
+def list_products(products)
+    message "Lista de Produtos:"
+
+    products.each do |product|
+        
+    end
+end
